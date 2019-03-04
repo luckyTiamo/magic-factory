198b548dad902826b65bf2bb8d0db2f781dfc197
甄鑫 <396220769@qq.com>
2019-03-04 20:04:06 +0800

    swiper.js

--- swiper/js/swiper.js
+++ swiper/js/swiper.js
@@ -1,5 +1,33 @@
-class Swiper {
-    constructor() {
-
+(function(){
+    var defaultSetting = {
+        "width": 600,
+        "height": 400
     }
-}
+    function Swiper(options) {
+        var self = this;
+        this.setting = Object.assign(defaultSetting, options);
+        this.container = document.getElementById('swiperContainer');
+        this.list = this.container.getElementByTagName('ul')[0];
+        this.prev = document.getElementById('swiperPrev');
+        this.next = document.getElementById('swiperNext');
+
+        this.prev.onclick = function () {
+            self.move('left');
+        }
+        this.next.onclick = function () {
+            self.move('right');
+        }
+        this.left = 0;
+    }
+    Swiper.prototype = {
+        move: function(direction) {
+            var self = this;
+            if (direction === 'right') {
+                
+            }
+        }
+    }
+
+    window.Swiper = Swiper;
+}(window));
+
