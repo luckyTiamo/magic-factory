9742ef07efa5e0dca8c52a86ea2ec7d6df72173f
甄鑫 <396220769@qq.com>
2019-03-05 22:34:07 +0800

    feature(swiper): 左右切换

--- swiper/js/swiper.js
+++ swiper/js/swiper.js
@@ -7,9 +7,16 @@
         var self = this;
         this.setting = Object.assign(defaultSetting, options);
         this.container = document.getElementById('swiperContainer');
-        this.list = this.container.getElementByTagName('ul')[0];
+        this.list = this.container.getElementsByTagName('ul')[0];
+        this.length = this.container.getElementsByClassName('swiper-item').length;
         this.prev = document.getElementById('swiperPrev');
         this.next = document.getElementById('swiperNext');
+
+        this.pagination = document.getElementsByClassName('pagination');
+
+        this.left = 0;
+        this.offsetX = this.setting.width;
+        this.index = 0;
 
         this.prev.onclick = function () {
             self.move('left');
@@ -17,14 +24,27 @@
         this.next.onclick = function () {
             self.move('right');
         }
-        this.left = 0;
     }
     Swiper.prototype = {
         move: function(direction) {
             var self = this;
             if (direction === 'right') {
-                
+                if (self.index === self.length - 1) {
+                    return;
+                }
+                self.left += self.offsetX;
+                self.index++;
+            } else {
+                if(self.index === 0) {
+                    return;
+                }
+                self.left -= self.offsetX;
+                self.index--;
             }
+            self.list.style.transform = 'translateX('+ -self.left + 'px)';
+        },
+        highlightBullet: function() {
+            
         }
     }
 
