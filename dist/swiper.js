b35c894e6c2796b9ebc3a1aef721ec2f71e45bc2
甄鑫 <tiamo1949@outlook.com>
2019-03-28 18:55:16 +0800

    缩略图

--- dist/swiper.js
+++ dist/swiper.js
@@ -18,14 +18,25 @@ var Swiper = function () {
 
         this.setting = Object.assign(defaultSetting, options);
         this.container = document.getElementById(this.setting.container);
+        this.swiperMain = this.container.parentNode;
         this.list = this.container.getElementsByTagName('ul')[0];
         this.swiperItem = this.container.getElementsByClassName('swiper-item');
         this.length = this.swiperItem.length;
         this.prev = document.getElementById(this.setting.prevBtn);
         this.next = document.getElementById(this.setting.nextBtn);
 
-        var pagination = this.setting.pagination;
-        this.pagination = document.querySelector(pagination);
+        this.pagination = document.querySelector(this.setting.pagination);
+
+        this.thumbs = document.getElementById(this.setting.thumbs.el);
+        this.thumbRect = this.thumbs.getElementsByClassName('rectangle')[0];
+        this.thumbsLength = this.setting.thumbs.num;
+        this.thumbsItem = this.thumbs.getElementsByClassName('thumbs-item');
+        this.thumbsItemWidth = ((this.setting.width - (this.thumbsLength - 1) * 10) / this.thumbsLength).toFixed(2);
+
+        for (var i = 0; i < this.thumbsItem.length; i++) {
+            this.thumbsItem[i].style.width = this.thumbsItemWidth + 'px';
+        }
+        this.thumbRect.style.width = this.thumbsItemWidth + 'px';
 
         // 动态创建dom
         for (var _i = 0; _i < this.length; _i++) {
@@ -56,19 +67,26 @@ var Swiper = function () {
             _this.moveNext();
         }, false);
 
-        this.container.addEventListener('mouseover', function () {
+        this.swiperMain.addEventListener('mouseover', function () {
             _this.stopPlay();
         }, false);
-        this.container.addEventListener('mouseout', function () {
+        this.swiperMain.addEventListener('mouseout', function () {
             _this.autoPlay();
         }, false);
 
         //  第几个按钮点击，还有更好的写法吗？
         var self = this;
-        for (var i = 0; i < this.bullet.length; i++) {
-            this.bullet[i].index = i;
-            this.bullet[i].onclick = function () {
-                self.moveToIndex(this.index + 1);
+        for (var _i2 = 0; _i2 < this.bullet.length; _i2++) {
+            this.bullet[_i2].index = _i2;
+            this.bullet[_i2].onclick = function () {
+                self.moveToIndex(this.index);
+            };
+        }
+
+        for (var _i3 = 0; _i3 < this.thumbsLength; _i3++) {
+            this.thumbsItem[_i3].index = _i3;
+            this.thumbsItem[_i3].onclick = function () {
+                self.selectThumb(this.index);
             };
         }
     }
@@ -82,7 +100,7 @@ var Swiper = function () {
             this.list.insertBefore(copyLastItem, this.swiperItem[0]);
             this.initNum = 1;
             this.left = this.initNum * this.offsetX;
-            this.list.style.left = -this.left + 'px';
+            this.list.style.transform = "translateX(" + -this.left + "px)";
             this.autoPlay();
         }
     }, {
@@ -111,38 +129,40 @@ var Swiper = function () {
                     this.initNum = this.length;
                 }
                 this.list.style.transition = "none";
-                this.list.style.left = -this.initNum * this.offsetX + 'px';
+                this.list.style.transform = "translateX(" + -this.initNum * this.offsetX + "px)";
             }
             this.initNum = this.initNum + num;
             this.left = this.initNum * this.offsetX;
             setTimeout(function () {
-                _this2.list.style.transition = "all .8s linear";
-                _this2.list.style.left = -_this2.left + 'px';
+                _this2.list.style.transition = "all .8s ease";
+                _this2.list.style.transform = "translateX(" + -_this2.left + "px)";
             }, 20);
         }
     }, {
         key: "moveToIndex",
         value: function moveToIndex(index) {
+            var currentIndex = index + 1;
             var currentNum = this.initNum;
             var diff = 0;
             if (currentNum === this.length + 1) {
                 currentNum = 1;
             }
-            if (currentNum === index) {
+            if (currentNum === currentIndex) {
                 return;
-            } else if (index > currentNum) {
-                diff = index - currentNum;
+            } else if (currentIndex > currentNum) {
+                diff = currentIndex - currentNum;
             } else {
-                diff = Math.abs(currentNum - this.length) + index;
+                diff = Math.abs(currentNum - this.length) + currentIndex;
             }
             this.moveStep(diff);
-            this.highlightBullet(index);
+            this.highlightBullet(currentIndex);
         }
     }, {
         key: "highlightBullet",
         value: function highlightBullet(num) {
             this.pagination.getElementsByClassName('active')[0].classList.remove('active');
             this.pagination.children[num - 1].classList.add('active');
+            this.selectThumb(num - 1);
         }
     }, {
         key: "autoPlay",
@@ -157,6 +177,12 @@ var Swiper = function () {
         key: "stopPlay",
         value: function stopPlay() {
             clearInterval(timer);
+        }
+    }, {
+        key: "selectThumb",
+        value: function selectThumb(index) {
+            this.thumbRect.style.left = this.thumbsItem[index].offsetLeft + 'px';
+            this.moveToIndex(index);
         }
     }]);
 
--- swiper/js/swiper2.js
+++ swiper/js/swiper2.js
@@ -7,14 +7,26 @@ class Swiper {
     constructor(options) {
         this.setting = Object.assign(defaultSetting, options);
         this.container = document.getElementById(this.setting.container);
+        this.swiperMain = this.container.parentNode;
         this.list = this.container.getElementsByTagName('ul')[0];
         this.swiperItem = this.container.getElementsByClassName('swiper-item');
         this.length = this.swiperItem.length;
         this.prev = document.getElementById(this.setting.prevBtn);
         this.next = document.getElementById(this.setting.nextBtn);
 
-        const pagination = this.setting.pagination;
-        this.pagination = document.querySelector(pagination);
+        this.pagination = document.querySelector(this.setting.pagination);
+
+        this.thumbs = document.getElementById(this.setting.thumbs.el);
+        this.thumbRect = this.thumbs.getElementsByClassName('rectangle')[0];
+        this.thumbsLength = this.setting.thumbs.num;
+        this.thumbsItem = this.thumbs.getElementsByClassName('thumbs-item');
+        this.thumbsItemWidth = ((this.setting.width - (this.thumbsLength - 1) * 10) / this.thumbsLength).toFixed(2);
+
+        for (let i = 0; i < this.thumbsItem.length; i++) {
+            this.thumbsItem[i].style.width = this.thumbsItemWidth + 'px';
+        }
+        this.thumbRect.style.width = this.thumbsItemWidth + 'px';
+    
 
         // 动态创建dom
         for (let i = 0; i < this.length; i++) {
@@ -46,19 +58,26 @@ class Swiper {
             this.moveNext();
         }, false);
 
-        this.container.addEventListener('mouseover', () => {
+        this.swiperMain.addEventListener('mouseover', () => {
             this.stopPlay();
         }, false);
-        this.container.addEventListener('mouseout', () => {
+        this.swiperMain.addEventListener('mouseout', () => {
             this.autoPlay();
         }, false);
 
         //  第几个按钮点击，还有更好的写法吗？
         const self = this;
-        for (var i = 0; i < this.bullet.length; i++) {
+        for (let i = 0; i < this.bullet.length; i++) {
             this.bullet[i].index = i;
             this.bullet[i].onclick = function () {
-                self.moveToIndex(this.index + 1);
+                self.moveToIndex(this.index);
+            }
+        }
+
+        for (let i = 0; i < this.thumbsLength; i++) {
+            this.thumbsItem[i].index = i;
+            this.thumbsItem[i].onclick = function () {
+                self.selectThumb(this.index);
             }
         }
     }
@@ -70,7 +89,7 @@ class Swiper {
         this.list.insertBefore(copyLastItem, this.swiperItem[0]);
         this.initNum = 1;
         this.left = this.initNum * this.offsetX;
-        this.list.style.left = - this.left + 'px';
+        this.list.style.transform = `translateX(${-this.left}px)`;
         this.autoPlay();
     }
 
@@ -94,36 +113,38 @@ class Swiper {
                 this.initNum = this.length;
             }
             this.list.style.transition = "none";
-            this.list.style.left = - this.initNum * this.offsetX + 'px';
+            this.list.style.transform = `translateX(${- this.initNum * this.offsetX}px)`;
         }
         this.initNum = this.initNum + num;
         this.left = this.initNum * this.offsetX;
         setTimeout(() => {
-            this.list.style.transition = "all .8s linear";
-            this.list.style.left = -this.left + 'px';
+            this.list.style.transition = "all .8s ease";
+            this.list.style.transform = `translateX(${-this.left}px)`;
         }, 20);
     }
 
     moveToIndex(index) {
+        let currentIndex = index + 1;
         let currentNum = this.initNum;
         let diff = 0;
         if (currentNum === this.length + 1) {
             currentNum = 1;
         }
-        if (currentNum === index) {
+        if (currentNum === currentIndex) {
             return;
-        } else if (index > currentNum) {
-            diff = index - currentNum;
+        } else if (currentIndex > currentNum) {
+            diff = currentIndex - currentNum;
         } else {
-            diff = Math.abs(currentNum - this.length) + index;            
+            diff = Math.abs(currentNum - this.length) + currentIndex;            
         }
         this.moveStep(diff);
-        this.highlightBullet(index);
+        this.highlightBullet(currentIndex);
     }
 
     highlightBullet(num) {
         this.pagination.getElementsByClassName('active')[0].classList.remove('active');
         this.pagination.children[num - 1].classList.add('active');
+        this.selectThumb(num - 1);
     }
 
     autoPlay() {
@@ -134,5 +155,10 @@ class Swiper {
 
     stopPlay() {
         clearInterval(timer);
+    }
+
+    selectThumb(index) {
+        this.thumbRect.style.left = this.thumbsItem[index].offsetLeft + 'px';
+        this.moveToIndex(index);
     }
 }
